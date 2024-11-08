3d4777f1e23d79943017ac85214623f2fdc44a0c
Alexander Silva Lima <lima.alexander.las@gmail.com>
2024-11-08 20:00:54 -0300

    Add Animal screen and button function

---
+++ demo/src/main/java/org/example/demo/MenuController.java
@@ -0,0 +1,30 @@
+package org.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuController {
+
+    @FXML
+    private Button animal;
+
+    @FXML
+    void abrirTelaAnimal(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("menu-view.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Cadastrar Grupo");
+        stage.setScene(new Scene(root));
+        stage.show();
+        }
+    }
+
